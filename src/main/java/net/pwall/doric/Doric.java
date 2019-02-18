463f97d603d788bd58bbed53715ae6cd39bef8b0
Peter Wall <pwall@pwall.net>
2019-02-18 21:56:30 +1100

    Some rearrangement of classes

--- src/main/java/net/pwall/doric/Doric.java
+++ src/main/java/net/pwall/doric/Doric.java
@@ -1,18 +1,32 @@
 /*
  * @(#) Doric.java
+ *
+ * doric Column-oriented database system
+ * Copyright (c) 2019 Peter Wall
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 package net.pwall.doric;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileWriter;
-import java.util.HashMap;
 
-import net.pwall.json.JSONFormat;
-import net.pwall.json.JSONObject;
-import net.pwall.util.CSV;
-import net.pwall.util.Strings;
 import net.pwall.util.UserError;
 
 /**
@@ -21,8 +35,6 @@ import net.pwall.util.UserError;
  * @author  Peter Wall
  */
 public class Doric {
-
-    private static final double[] decimalShifts = { 1, 10, 100, 1000, 10000 };
 
     public static void main(String[] args) {
         try {
@@ -81,148 +93,9 @@ public class Doric {
                 headers = Boolean.FALSE;
             if (showMetadata == null)
                 showMetadata = Boolean.FALSE;
-            CSV csv = new CSV(new FileInputStream(csvFile));
-            Table table = new Table();
-            table.setSource(csvFile.getCanonicalPath());
-            if (maxUnique != null)
-                table.setMaxUniqueValues(maxUnique);
-            table.analyse(csv, headers);
-
-            // now write to files
-
-            if (outFile != null) {
-                int columnCount = table.getColumnCount();
-                ColumnWriter[] columnWriters = new ColumnWriter[columnCount];
-                ColumnWriter[] columnDataWriters = new ColumnWriter[columnCount];
-                UniqueValueMap[] uniqueValueMaps = new UniqueValueMap[columnCount];
-                if (!outFile.exists() && !outFile.mkdirs())
-                    throw new UserError("Error creating output directory");
-
-                // open files
-
-                for (int i = 0; i < columnCount; i++) {
-                    Table.Column column = table.getColumn(i);
-                    Table.StorageType storageType = column.getStorageType();
-                    if (!(storageType == Table.StorageType.none ||
-                            storageType == Table.StorageType.fixed)) {
-                        StringBuilder sb = new StringBuilder();
-                        Strings.append3Digits(sb, i);
-                        sb.append(".out");
-                        String fileName = sb.toString();
-                        column.setFilename(fileName);
-                        columnWriters[i] = new ColumnWriter(outFile, fileName);
-                        if (storageType == Table.StorageType.bytes) {
-                            sb.setLength(0);
-                            Strings.append3Digits(sb, i);
-                            sb.append("a.out");
-                            String dataFilename = sb.toString();
-                            column.setDataFilename(dataFilename);
-                            columnDataWriters[i] = new ColumnWriter(outFile, dataFilename);
-                            if (column.getNumUniqueValues() > 0)
-                                uniqueValueMaps[i] = new UniqueValueMap();
-                        }
-                    }
-                }
-
-                csv = new CSV(new FileInputStream(csvFile));
-                if (headers)
-                    csv.next();
-                while (csv.hasNext()) {
-                    CSV.Record record = csv.next();
-                    for (int i = 0; i < columnCount; i++) {
-                        String str = record.getField(i);
-                        Table.Column column = table.getColumn(i);
-                        ColumnWriter writer = columnWriters[i];
-                        Table.StorageType storageType = column.getStorageType();
-                        int decimalShift = column.getDecimalShift();
-                        if (storageType == Table.StorageType.int8) {
-                            if (decimalShift > 0) {
-                                double d = Double.valueOf(str) * decimalShifts[decimalShift];
-                                writer.writeInt8((int)Math.round(d));
-                            }
-                            else {
-                                writer.writeInt8(Integer.valueOf(str));
-                            }
-                        }
-                        else if (storageType == Table.StorageType.int16) {
-                            if (decimalShift > 0) {
-                                double d = Double.valueOf(str) * decimalShifts[decimalShift];
-                                writer.writeInt16((int)Math.round(d));
-                            }
-                            else {
-                                writer.writeInt16(Integer.valueOf(str));
-                            }
-                        }
-                        else if (storageType == Table.StorageType.int32) {
-                            if (decimalShift > 0) {
-                                double d = Double.valueOf(str) * decimalShifts[decimalShift];
-                                writer.writeInt32((int)Math.round(d));
-                            }
-                            else {
-                                writer.writeInt32(Integer.valueOf(str));
-                            }
-                        }
-                        else if (storageType == Table.StorageType.int64) {
-                            if (decimalShift > 0) {
-                                double d = Double.valueOf(str) * decimalShifts[decimalShift];
-                                writer.writeInt64(Math.round(d));
-                            }
-                            else {
-                                writer.writeInt64(Long.valueOf(str));
-                            }
-                        }
-                        else if (storageType == Table.StorageType.bytes) {
-                            // TODO confirm size of offset and length written to writer
-                            long start;
-                            long length;
-                            if (column.getNumUniqueValues() > 0) {
-                                Long uniqueValueCached = uniqueValueMaps[i].get(str);
-                                if (uniqueValueCached != null) {
-                                    start = uniqueValueCached;
-                                    length = uniqueValueCached >> 32;
-                                }
-                                else {
-                                    ColumnWriter dataWriter = columnDataWriters[i];
-                                    start = dataWriter.getOffset();
-                                    dataWriter.writeBytes(str);
-                                    length = dataWriter.getOffset() - start;
-                                    uniqueValueMaps[i].put(str, (length << 32) + start);
-                                }
-                            }
-                            else {
-                                ColumnWriter dataWriter = columnDataWriters[i];
-                                start = dataWriter.getOffset();
-                                dataWriter.writeBytes(str);
-                                length = dataWriter.getOffset() - start;
-                            }
-                            writer.writeInt32((int)start);
-                            writer.writeInt16((int)length);
-                        }
-                        else if (!(storageType == Table.StorageType.none ||
-                                storageType == Table.StorageType.fixed)) {
-                            throw new RuntimeException("Unhandled data type");
-                        }
-                    }
-                }
-                for (int i = 0; i < columnCount; i++) {
-                    ColumnWriter writer = columnWriters[i];
-                    if (writer != null)
-                        writer.close();
-                    writer = columnDataWriters[i];
-                    if (writer != null)
-                        writer.close();
-                }
-                JSONObject json = table.toJSON();
-                FileWriter metadata = new FileWriter(new File(outFile, "metadata.json"));
-                json.appendJSON(metadata);
-                metadata.close();
-
-            }
-
-            if (showMetadata) {
-                JSONFormat format = new JSONFormat();
-                System.out.println(format.format(table.toJSON()));
-            }
+            if (outFile != null && !outFile.exists() && !outFile.mkdirs())
+                throw new UserError("Error creating output directory");
+            Ingestor.ingest(csvFile, headers, outFile, showMetadata, maxUnique);
         }
         catch (Exception e) {
             e.printStackTrace();
@@ -237,10 +110,6 @@ public class Doric {
         if (result.startsWith("-"))
             throw new UserError(msg);
         return result;
-    }
-
-    private static class UniqueValueMap extends HashMap<String, Long> {
-
     }
 
 }
--- src/main/java/net/pwall/doric/Table.java
+++ src/main/java/net/pwall/doric/Table.java
@@ -1,15 +1,34 @@
 /*
  * @(#) Table.java
+ *
+ * doric Column-oriented database system
+ * Copyright (c) 2019 Peter Wall
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 
 package net.pwall.doric;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Set;
 
 import net.pwall.json.JSONArray;
 import net.pwall.json.JSONObject;
@@ -64,13 +83,13 @@ public class Table {
                 throw new IllegalArgumentException("CSV Header line missing");
             CSV.Record header = csv.next();
             for (int i = 0, n = header.getWidth(); i < n; i++)
-                columns.add(new Column(header.getField(i)));
+                columns.add(new Column(this, header.getField(i)));
         }
         while (csv.hasNext()) {
             CSV.Record record = csv.next();
             if (!readHeader && rowCount == 0) {
                 for (int i = 0, n = record.getWidth(); i < n; i++)
-                    columns.add(new Column("col" + i));
+                    columns.add(new Column(this, "col" + i));
             }
 
             int width = record.getWidth();
@@ -157,283 +176,5 @@ public class Table {
         json.put("columns", array);
         return json;
     }
-
-    /**
-     * Inner class to represent the metadata for the column.
-     */
-    public class Column {
-
-        private String name;
-
-        private int minWidth;
-        private int maxWidth;
-
-        private boolean integer;
-        private boolean intInit;
-        private boolean floating;
-        private boolean floatInit;
-
-        private long maxInt;
-        private long minInt;
-        private double maxFloat;
-        private double minFloat;
-        private int maxDecimals;
-
-        private Set<String> uniqueValues;
-        // Implementation note - once the maximum is reached, this variable is set to null.
-        // That can be used to determine that the column has more than the maximum
-
-        private StorageType storageType;
-        private String value;
-        private int decimalShift;
-
-        private String filename;
-        private String dataFilename;
-
-        public Column(String name) {
-            this.name = name;
-            minWidth = Integer.MAX_VALUE;
-            maxWidth = 0;
-            integer = true;
-            intInit = false;
-            floating = true;
-            floatInit = false;
-            maxInt = 0;
-            minInt = 0;
-            maxFloat = 0.0;
-            minFloat = 0.0;
-            maxDecimals = 0;
-            uniqueValues = new HashSet<>();
-            storageType = StorageType.undetermined;
-            value = null;
-            decimalShift = 0;
-            filename = null;
-            dataFilename = null;
-        }
-
-        /**
-         * Analyse the contents of the string and make appropriate changes to the metadata for
-         * the column.
-         *
-         * @param   str     the contents of one row for this column
-         */
-        public void analyse(String str) {
-
-            int strWidth = str.length();
-            if (strWidth < minWidth)
-                minWidth = strWidth;
-            if (strWidth > maxWidth)
-                maxWidth = strWidth;
-
-            if (strWidth == 0)
-                return; // empty column doesn't affect following analysis
-
-            if (floating) {
-                try {
-                    double d = Double.valueOf(str);
-                    if (floatInit) {
-                        if (d < minFloat)
-                            minFloat = d;
-                        if (d > maxFloat)
-                            maxFloat = d;
-                    }
-                    else {
-                        minFloat = d;
-                        maxFloat = d;
-                        floatInit = true;
-                    }
-                    int i = str.indexOf('.');
-                    if (i >= 0) {
-                        int decimals = 0;
-                        while (++i < str.length()) {
-                            char ch = str.charAt(i);
-                            if (ch < '0' || ch > '9')
-                                break;
-                            decimals++;
-                        }
-                        if (decimals > maxDecimals)
-                            maxDecimals = decimals;
-                    }
-                }
-                catch (NumberFormatException nfe) {
-                    floating = false;
-                }
-            }
-
-            if (integer) {
-                try {
-                    long i = Long.valueOf(str);
-                    if (intInit) {
-                        if (i < minInt)
-                            minInt = i;
-                        if (i > maxInt)
-                            maxInt = i;
-                    }
-                    else {
-                        minInt = i;
-                        maxInt = i;
-                        intInit = true;
-                    }
-                }
-                catch (NumberFormatException nfe) {
-                    integer = false;
-                }
-            }
-
-            if (uniqueValues != null) {
-                if (uniqueValues.size() <= maxUniqueValues)
-                    uniqueValues.add(str);
-                if (uniqueValues.size() > maxUniqueValues)
-                    uniqueValues = null;
-            }
-
-            // At this point, we can check for other data types, like dates, times, currency
-            // codes, IATA city codes, ...
-        }
-
-        /**
-         * Work out the storage requirements for the column.
-         */
-        public void resolve() {
-            if (maxWidth == 0) {
-                storageType = StorageType.none;
-            }
-            if (getNumUniqueValues() == 1) {
-                storageType = StorageType.fixed;
-                value = uniqueValues.iterator().next();
-            }
-            else if (integer) {
-                storageType = getIntStorageType(minInt, maxInt);
-            }
-            else if (floating) {
-                if (maxDecimals <= maxDecimalShift) {
-                    long minShifted = Math.round(minFloat * decimalShifts[maxDecimals]);
-                    long maxShifted = Math.round(maxFloat * decimalShifts[maxDecimals]);
-                    storageType = getIntStorageType(minShifted, maxShifted);
-                    decimalShift = maxDecimals;
-                }
-                else
-                    storageType = StorageType.float64;
-            }
-            else {
-                storageType = StorageType.bytes;
-            }
-        }
-
-        private StorageType getIntStorageType(long min, long max) {
-            if (min >= Byte.MIN_VALUE && max <= Byte.MAX_VALUE)
-                return StorageType.int8;
-            if (min >= Short.MIN_VALUE && max <= Short.MAX_VALUE)
-                return StorageType.int16;
-            if (min >= Integer.MIN_VALUE && max <= Integer.MAX_VALUE)
-                return StorageType.int32;
-            return StorageType.int64;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public int getMinWidth() {
-            return minWidth;
-        }
-
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getMaxWidth() {
-            return maxWidth;
-        }
-
-        public boolean isInteger() {
-            return integer;
-        }
-
-        public long getMinInt() {
-            return minInt;
-        }
-
-        public long getMaxInt() {
-            return maxInt;
-        }
-
-        public boolean isFloating() {
-            return floating;
-        }
-
-        public double getMinFloat() {
-            return minFloat;
-        }
-
-        public double getMaxFloat() {
-            return maxFloat;
-        }
-
-        public int getMaxDecimals() {
-            return maxDecimals;
-        }
-
-        public int getDecimalShift() {
-            return decimalShift;
-        }
-
-        public int getNumUniqueValues() {
-            return uniqueValues == null ? 0 : uniqueValues.size();
-        }
-
-        public String getFilename() {
-            return filename;
-        }
-
-        public void setFilename(String filename) {
-            this.filename = filename;
-        }
-
-        public String getDataFilename() {
-            return dataFilename;
-        }
-
-        public void setDataFilename(String dataFilename) {
-            this.dataFilename = dataFilename;
-        }
-
-        public StorageType getStorageType() {
-            return storageType;
-        }
-
-        public JSONObject toJSON() {
-            JSONObject json = JSONObject.create().putValue("name", name);
-            if (rowCount > 0) {
-                json.putValue("minWidth", minWidth).putValue("maxWidth", maxWidth);
-                if (isInteger()) {
-                    json.putValue("type", "integer").putValue("minInt", minInt).
-                            putValue("maxInt", maxInt);
-                }
-                else if (isFloating()) {
-                    json.putValue("type", "float").putValue("minFloat", minFloat).
-                            putValue("maxFloat", maxFloat).putValue("maxDecimals", maxDecimals);
-                }
-                else
-                    json.putValue("type", "undetermined");
-                int numUnique = getNumUniqueValues();
-                if (numUnique > 0)
-                    json.putValue("uniqueValues", numUnique);
-                json.putValue("storageType", storageType.toString());
-                if (storageType == StorageType.fixed)
-                    json.putValue("value", value);
-                if (decimalShift != 0)
-                    json.putValue("decimalShift", decimalShift);
-                if (filename != null)
-                    json.putValue("filename", filename);
-                if (dataFilename != null)
-                    json.putValue("dataFilename", dataFilename);
-            }
-            return json;
-        }
-
-    }
-
-    public enum StorageType { undetermined, none, fixed, int8, int16, int32, int64, float64, bytes }
 
 }
